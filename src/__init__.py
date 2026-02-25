a4f09279214891c0d8bf885fe689854acd66bfc9
benitez1181 <benitez_marcos29@outlook.com>
2026-02-25 17:33:32 -0600

    Bug

--- src/__init__.py
+++ src/__init__.py
@@ -1,3 +1,3 @@
-from .data_loader import get_test_data, get_train_data
+from .data_loader import get_val_data, get_train_data
 from .data_organizer import organize_data
 from .create_subdirectory import subdirectories
