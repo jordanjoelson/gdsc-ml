aa4d45c38879719179dd672612ef48cfc50db112
benitez1181 <benitez_marcos29@outlook.com>
2026-02-26 20:01:35 -0600

    Data Augmentation Implementation

--- models/model.py
+++ models/model.py
@@ -2,10 +2,20 @@ import tensorflow as tf
 from tensorflow.keras import layers
 
 def get_model():
+    IMG_SIZE = 380
+
+    augmentation = tf.keras.Sequential([
+        layers.RandomFlip("horizontal"),
+        layers.RandomRotation(0.2)
+    ])
+
+    input = tf.keras.Input(shape=(IMG_SIZE, IMG_SIZE, 3))
+    x = augmentation(input)
+
     model = tf.keras.applications.EfficientNetB4(
             include_top=False,
             weights='imagenet',
-            input_shape=(380, 380, 3)
+            input_tensor=x
     )
     
     x = layers.GlobalAveragePooling2D()(model.output)
