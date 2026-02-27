69d1bbe49bafcdf423db875786648f5b3306992c
benitez1181 <benitez_marcos29@outlook.com>
2026-02-27 00:17:51 -0600

    Model changes

--- models/model.py
+++ models/model.py
@@ -11,13 +11,15 @@ def get_model():
 
     input = tf.keras.Input(shape=(IMG_SIZE, IMG_SIZE, 3))
     x = augmentation(input)
+    x = tf.keras.applications.efficientnet.preprocess_input(x)
 
     model = tf.keras.applications.EfficientNetB4(
             include_top=False,
             weights='imagenet',
             input_tensor=x
     )
-    
+    model.trainable = False
+
     x = layers.GlobalAveragePooling2D()(model.output)
     output = layers.Dense(7, activation="softmax")(x)
 
